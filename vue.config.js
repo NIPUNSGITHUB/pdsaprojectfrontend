22ca5ad6c6f33b3c6e714923705b40a483cd1c77
Nipun Ishara <nipunishara.info@gmail.com>
2020-04-23 22:52:07 +0530

    proxy server update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
+
     devServer: {
-        proxy: 'http://localhost:5000/api',
+        proxy:'http://pdsa-group4.azurewebsites.net/api',
+        // proxy: 'http://localhost:5000/api',
     }
-  }
+}
